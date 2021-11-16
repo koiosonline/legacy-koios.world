32fe46fbd1becbec87f59856fc1e2c42ab3b10e4
Rowin206 <rowinvanamsterdam@gmail.com>
2021-11-16 19:49:26 +0100

    Changed footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,10 @@
 const Footer = () => {
-    return (
-        <footer className={'footer'}>
-            <p>We are experimenting. A new everything in the coming months.</p>
-               <p> Buidling on the shoulder of giants</p>
-            <div className={'footer__underline'}/>
-        </footer>
-    )
-}
+  return (
+    <footer className={"footer"}>
+      <p>building on the shoulders of Titans.</p>
+      <div className={"footer__underline"} />
+    </footer>
+  );
+};
 
 export default Footer;
