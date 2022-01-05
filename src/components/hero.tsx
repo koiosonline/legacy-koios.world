d6fe2934958f7efbefcc6b16fb77375c4e404fc5
Stephan Kop <stephankop@stephans-mbp.home>
2022-01-05 17:59:48 +0100

    made treemap clickable and small changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,11 +4,11 @@ const Hero = (props: { title: string; description: string;}) => {
       <h1 className={'hero__title'}>{props.title}</h1>
       <p className={'hero__description'}>{props.description}</p>
       <div className={'hero__links'}>
-        <a href={'https://app.koios.world/'} className={'link'}>
+        <a href={'https://app.koios.world/'} className={'link'} target="_blank" rel="noreferrer">
           <img src={'/images/Learn.svg'} alt="article"/>
           <p className={'link__text'}>Learn</p>
         </a>
-        <a href={'https://app.koios.world/#/earn'} className={'link'}>
+        <a href={'https://app.koios.world/#/earn'} className={'link'} target="_blank" rel="noreferrer">
           <img src={'/images/earn.svg'} alt="article"/>
           <p className={'link__text'}>Earn</p>
         </a>
